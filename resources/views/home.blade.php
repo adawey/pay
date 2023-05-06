66bffd71654d75455a302262774ed97d8b8a880d
MS <mo7ammed.sabre@gmail.com>
2023-05-07 01:00:24 +0300

    edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,28 +8,27 @@
         </div>
     @endif
     <div class="container-fluid page_content" style="min-height: 100vh;">
-       
+
             <div class="container">
-                <div class="row">
-                <div style="text-align: center" class="col-md-6 col-12">
-                    <h1> {{ Auth::user()->balance }} </h1>
-                    <h5> my balance </h5>
+                <div class="row d-flex justify-content-center mt-5 mx-3">
+                    <div class="col-md-6 col-12 text-center" style="border:1px solid #70d4d4; border-radius:15px; ">
+                        <h1> {{ Auth::user()->balance }} </h1>
+                        <p class="h5 text-secondary"> Current Balance </p>
                 </div>
-                <div class="my-5" style="display: flex;align-items: center;justify-content: center;">
-                    <a href="{{route('AddBalancePage')}}" type="button" class="btn btn-outline-success py-3 px-5">Add Money</a>
+                <div class="mt-5" style="display: flex;align-items: center;justify-content: center;">
+                    <a href="{{route('AddBalancePage')}}" type="button" class="btn btn-outline-success  py-3 px-4 fs-4 fw-bold">Recharge Money</a>
                 </div>
-            
-                <div class="my-5" style="display: flex;align-items: center;justify-content: center;">
-                    <span id="pay_button" class="submit_btn"
-                        style="background-color:#46B5B5;padding: 5px 30px;cursor: pointer;"> <a href="{{route('sendMonyPage')}}"> send mony </a> </span>
+
+                <div class="mt-3" style="display: flex;align-items: center;justify-content: center;">
+                    <a href="{{route('sendMonyPage')}}" type="button" class="btn btn-outline-success py-3 px-5 fs-4 fw-bold">Send Money</a>
                 </div>
-                <div class="my-5" style="display: flex;align-items: center;justify-content: center;">
-                    <span id="pay_button" class="submit_btn"
-                        style="background-color:#46B5B5;padding: 5px 30px;cursor: pointer;"> <a href="{{route('reportPage')}}"> send report </a> </span>
+
+                <div class="mt-3" style="display: flex;align-items: center;justify-content: center;">
+                    <a href="{{route('reportPage')}}" type="button" class="btn btn-outline-success py-3 px-5 fs-4 fw-bold">Send Report</a>
                 </div>
-                <div class="my-5" style="display: flex;align-items: center;justify-content: center;">
-                    <span id="pay_button" class="submit_btn"
-                        style="background-color:#46B5B5;padding: 5px 30px;cursor: pointer;"> <a href="{{route('myPayment')}}">myPayment </a> </span>
+
+                <div class="mt-3" style="display: flex;align-items: center;justify-content: center;">
+                    <a href="{{route('myPayment')}}" type="button" class="btn btn-outline-success py-3 px-5 fs-4 fw-bold">My Payment</a>
                 </div>
             </div>
         </div>
