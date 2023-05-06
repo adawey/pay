1de1426653be1d0617297abee9e96b3e61126aab
MS <mo7ammed.sabre@gmail.com>
2023-05-07 01:45:46 +0300

    edit

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -4,16 +4,17 @@
 
 <div class="container-fluid page_content" style="min-height: 100vh;">
     <form id="payment_form" class="form" method="get" action="verifyPage.php">
-        <div class="container">
+ 
 
-            <table class="table">
+           <div class="table-responsive text-center">
+            <table class="table table-bordered">
                 <thead>
                     <tr>
                         <!-- <th scope="col">#</th> -->
                         <th scope="col">payment_number</th>
                         <th scope="col">amount</th>
                         <th scope="col">status</th>
-                        <th scope="col">type </th>
+                        <th scope="col">type</th>
                         <th scope="col">notes</th>
                         <th scope="col">action</th>
                     </tr>
@@ -28,8 +29,10 @@
                         <td>{{ $payment->note }}</td>
                         <td>
                             @if($payment->status == "pending" &&  $payment->type == "sender")
-                            <button type="button" class="btn btn-success">Success</button>
-                            <button type="button" class="btn btn-danger">Danger</button>
+                            <div class="d-flex justify-content-between">
+                            <button type="button" class="btn btn-success px-2">Approve</button>
+                            <button type="button" class="btn btn-danger ">Decline</button>
+                            </div>
                             @endif
                         </td>
                     </tr>
--- resources/views/user/send.blade.php
+++ resources/views/user/send.blade.php
@@ -7,24 +7,26 @@
         @csrf
         <div class="container">
             
-            <div class="row">
-                <div class="col-md-6  mt-4">
-                    <div class="input_box" style="border: none;box-shadow: none;">
+            <div class="row mx-3">
+                <div class="col-md-6 w-100 mt-4">
+                    <div class="input_box " style="border: none;box-shadow: none;">
                         <input name="phone_number" type="text"  placeholder="phone_number ">
                     </div>
                 </div>
                
             </div>
-            <div class="row mt-4">
-                <div class="col-md-12">
-                    <select class="form-select" name="type" aria-label="Default select example">
-                        <option selected> select your pay Type    </option>
-                        <option value="1"> friendly </option>
-                        <option value="2"> salle some thing </option>
-                      </select>
+
+                <div class="row mx-3 mt-3">
+                    <div class="col-md-12 ">
+                        <select class="input p-3 w-100 fs-5" style="border: solid 1px #797fbb; box-shadow: none; border-radius: 10px ;" name="type" aria-label="Default select example" required>
+                            <option selected disabled>select payment Type   </option>
+                            <option value="1">Friendly</option>
+                            <option value="2">Sell Something</option>
+                          </select>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
+
+            <div class="row mx-3">
                 <div class="col-md-12">
                     <div class="input_box mt-3" style="border: none; box-shadow: none;">
                         <input type="number" title="transfer amount" name="amount"
@@ -32,9 +34,9 @@
                     </div>
                 </div>
             </div>
-            <div class="my-5" style="display: flex;align-items: center;justify-content: center;">
-                <button type="submit" >Confirm</button>
-            </div>
+                <div class="mt-3" style="display: flex;align-items: center;justify-content: center;">
+                    <a type="submit" class="btn btn-outline-success py-3 px-5 fs-4 fw-bold">Confirm</a>
+                </div>
         </div>
     </form>
 </div>
