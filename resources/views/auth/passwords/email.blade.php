3e85ef00cf3b22f6c31301bd9cbac98428952eac
MS <mo7ammed.sabre@gmail.com>
2023-05-06 19:35:52 +0300

    edit

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -44,5 +44,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
